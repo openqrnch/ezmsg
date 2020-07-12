a28190cc2c7a4a3053e7337363bd0cd0f0729023
Jan Danielsson <jan.danielsson@qrnch.com>
2020-07-13 00:03:42 +0200

    Add module documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+//! A "message", represented by the Msg struct, is an entity that is
+//! typically transferred over the network between clients/servers or peers.
+//! Each message consist of a mandatory "topic" (a utf-8 string) and zero or
+//! more parameters (each of which is a key/value pair (of utf-8 strings).
+//!
+//! ezmsg does not handle transmission; it only represents the message buffer
+//! and provides methods to serialize messages.
+
 mod err;
 mod message;
 mod validators;
